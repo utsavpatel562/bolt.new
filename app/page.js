d228844680d2b5d0cf677bb16fdfe43d47a8f2a2
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-05-25 14:43:10 -0400

    latest commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 // Code by Utsav Patel
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
+    <div className="">
     <h2>Utsav Patel</h2>
     <Button>Button</Button>
+    </div>
     </>
   );
 }
