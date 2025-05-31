9fa670bf3fda41406bb545b303fa69a4bcc12f22
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-05-31 10:19:47 -0400

    latest commit

--- components/customs/CodeView.jsx
+++ components/customs/CodeView.jsx
@@ -1,7 +1,25 @@
+"use client";
 import React from "react";
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+  SandpackFileExplorer,
+} from "@codesandbox/sandpack-react";
 
 function CodeView() {
-  return <div>CodeView</div>;
+  return (
+    <>
+      <SandpackProvider template="react" theme={"dark"}>
+        <SandpackLayout>
+          <SandpackFileExplorer style={{ height: "82vh" }} />
+          <SandpackCodeEditor />
+          <SandpackPreview />
+        </SandpackLayout>
+      </SandpackProvider>
+    </>
+  );
 }
 
 export default CodeView;
